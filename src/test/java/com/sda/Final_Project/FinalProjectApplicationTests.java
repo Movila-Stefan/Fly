b44ea5ee9c02124fe10f0c0abcec1c866e953d3b
Movila-Stefan <movila.vl@gmail.com>
2022-09-13 22:58:39 +0300

    first commit

---
+++ src/test/java/com/sda/Final_Project/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
